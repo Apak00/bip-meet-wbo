37bbb4866cf10bf4ca0ca42be7c81d4d5938644d
Mahmud Apak <mahmud.apak@turkcell.com.tr>
2021-06-02 14:32:08 +0300

    Changes for multi server

--- client-data/js/board.js
+++ client-data/js/board.js
@@ -62,9 +62,10 @@ Tools.connect = function () {
 		self.socket = null;
 	}
 
+	var boardName = window.location.pathname.split("/boards")[1] || window.location.pathname; 
 
 	this.socket = io.connect('', {
-		"path": window.location.pathname.split("/boards/")[0] + "/socket.io",
+		"path": "/annotation" + "/socket.io" + boardName,
 		"reconnection": true,
 		"reconnectionDelay": 100, //Make the xhr connections as fast as possible
 		"timeout": 1000 * 60 * 20 // Timeout after 20 minutes
